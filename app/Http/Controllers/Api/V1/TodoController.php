5cbca94403fa5f14e2a4e733fcda6d344dc95324
Ihor Rud <Igorryd7@gmail.com>
2023-02-24 22:07:34 +0200

    First Day

---
+++ app/Http/Controllers/Api/V1/TodoController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Todo\StoreTodoRequest;
+use App\Http\Requests\Todo\UpdateTodoRequest;
+use App\Http\Resources\Todo\TodoCollection;
+use App\Http\Resources\Todo\TodoResource;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        return TodoCollection::collection(Todo::all());
+    }
+
+    public function store(StoreTodoRequest $request)
+    {
+        Todo::create([
+            'title' => $request['title'],
+            'body' => $request['body'],
+            'user_id' => \PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth::user()->id,
+        ]);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Task successfully created!',
+        ]);
+    }
+
+    public function show(Todo $todo)
+    {
+        return new TodoResource($todo);
+    }
+
+    public function update(UpdateTodoRequest $request,Todo $todo)
+    {
+        $todo->update($request->validated());
+        return response()->json(['status' => 'success', 'message' => 'Task successfully updated!']);
+    }
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return response()->json(['status' => 'success','message' => 'Task successfully deleted']);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\V1\TodoController;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+Route::controller(TodoController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todos', 'store');
+    Route::get('todos/{todo}', 'show');
+    Route::put('todos/{todo}', 'update');
+    Route::delete('todos/{todo}', 'destroy');
+});
