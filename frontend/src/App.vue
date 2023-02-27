210318351e491187e8760f3978c1c62ecf1beab7
Ihor Rud <Igorryd7@gmail.com>
2023-02-27 20:39:52 +0200

    set up frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+import Todo from "@/components/Todo.vue";
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <h1 class="text-green-600 text-5xl center">TodoApp👋</h1>
+        </div>
+    </header>
+
+    <main>
+        <Todo/>
+    </main>
+</template>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="greetings">
+        <h1 class="text-green-600 text-5xl">{{ msg }}</h1>
+    </div>
+</template>
